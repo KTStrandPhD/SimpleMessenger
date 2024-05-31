671052a491148b2376915a1d2e2d648472aa6de0
KTStrandPhD <115662739+KTStrandPhD@users.noreply.github.com>
2024-05-30 20:54:20 -0500

    Update SimpleMessenger.cxx
    Formatting corrections

--- src/SimpleMessenger.cxx
+++ src/SimpleMessenger.cxx
@@ -10,7 +10,7 @@ void SimpleMessenger::DefineEnumMap() {
 }
 
 SimpleMessenger::height SimpleMessenger::RandomizeEnum() {
-	return static_cast<height>(rand() % (tall + 1));                  // (tall + 1) is used to guarantee that any of the three enum properties can be selected by RNG 
+	return static_cast<height>(rand() % (tall + 1));                  //(tall + 1) is used to guarantee that any of the three enum properties can be selected by RNG 
 }
 
 int SimpleMessenger::RandomizeInt() {
@@ -18,17 +18,17 @@ int SimpleMessenger::RandomizeInt() {
 }
 
 float SimpleMessenger::RandomizeFloat() {
-	return (float)rand() / RAND_MAX;									                // Selecting a random float between 0 and 1
+	return (float)rand() / RAND_MAX;			          //Selecting a random float between 0 and 1
  
 }
 
-std::string SimpleMessenger::RandomizeString() {										// Selects name from pre-built name array
+std::string SimpleMessenger::RandomizeString() {			  //Selects name from pre-built name array
 	int nameSelector = rand() % 20;
 	return names[nameSelector];
 }
 
 /* MessengerDemo Subclass Definitions */
-void SimpleMessenger::GenerateData() {                              //Randomizes and populates data object
+void SimpleMessenger::GenerateData() {                                    //Randomizes and populates data object
 	this->position = this->RandomizeInt();
 	this->velocity = this->RandomizeFloat();
 	this->name = this->RandomizeString();
